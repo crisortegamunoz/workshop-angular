07c7f39274a0bb3a185fce8f965a3183b021cc09
CrisOrtega <crisortegamunoz@gmail.com>
2025-01-21 13:50:29 -0300

    ultimos ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  characters:any = [
+    {
+      "id": 0,
+      "firstName": "Daenerys",
+      "lastName": "Targaryen",
+      "fullName": "Daenerys Targaryen",
+      "title": "Mother of Dragons",
+      "family": "House Targaryen",
+      "image": "daenerys.jpg",
+      "imageUrl": "https://thronesapi.com/assets/images/daenerys.jpg"
+    },
+    {
+        "id": 2,
+        "firstName": "Jon",
+        "lastName": "Snow",
+        "fullName": "Jon Snow",
+        "title": "King of the North",
+        "family": "House Stark",
+        "image": "jon-snow.jpg",
+        "imageUrl": "https://thronesapi.com/assets/images/jon-snow.jpg"
+    }
+  ]
+
 }
